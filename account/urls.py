34f981a54a8ae4fa9bebac18b8508615ac86a8c3
teamsofteng <37052413+teamsofteng@users.noreply.github.com>
2018-04-08 16:18:15 -0500

    Add files via upload
    login and logout pages

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login, logout
 urlpatterns = [
-    url(r'^$', views.account),
+    	url(r'^$',views.home),
+	url(r'^login/$', login,{'template_name': 'account/index.html'}),
+	url(r'^logout/$', logout,{'template_name': 'account/index1.html'}),
 ]
