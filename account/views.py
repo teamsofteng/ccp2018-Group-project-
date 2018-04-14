bfa7092dc6628d6069f282d6e0f0ab99fcff5c3c
teamsofteng <37052413+teamsofteng@users.noreply.github.com>
2018-04-14 12:16:55 -0500

    Add files via upload
    customized the register page

--- account/views.py
+++ account/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from account.forms import RegistrationForm
 def home(request):
 	return render(request, 'account/Homepage.html')
 def register(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = RegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect('/account')
+			return redirect('http://ec2-35-173-183-216.compute-1.amazonaws.com/')
 	else:
-		form = UserCreationForm()
+		form = RegistrationForm()
 		
 
 		args = {'form': form}
