af05966ab983b4f7c23fc1b7bc0ee2ae684e6e47
teamsofteng <37052413+teamsofteng@users.noreply.github.com>
2018-04-15 15:43:40 -0500

    Add files via upload
    removed nodeweight variables

--- firstPro/classReview/models.py
+++ firstPro/classReview/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from scheduleGen.models import CourseLinks
+#class CourseLinks(models.Model):
+#    class Meta:
+#        unique_together = (('courseID','connCourseID'),)
+#    courseID = models.CharField(max_length=10)
+#    overallNode = models.FloatField()    
+#    connCourseID = models.CharField(max_length=10)                                                
+#    difficultyNode = models.FloatField()           
+    #difficultyNodeWeight = models.IntegerField()   
+#    cohesionNode = models.FloatField()             
+    #cohseionNodeWeight = models.IntegerField()   
--- firstPro/classReview/views.py
+++ firstPro/classReview/views.py
@@ -1,7 +1,23 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
-from scheduleGen.models import Courses, CourseLinks, Majors
+from django.shortcuts import render, redirect
+from classReview.forms import classReviewForm
+from classReview.models import CourseLinks
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
 class home(TemplateView):
-	template_name =  'classReview/index.html'
+    template_name =  'classReview/index.html'
+    def get(self,request):
+        form = classReviewForm()
+        return render(request,self.template_name, {'form': form})
+    def post(self, request):
+        form = classReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+				
+        
+        return render(request, self.template_name, {'form': form,})
+			
+
 
