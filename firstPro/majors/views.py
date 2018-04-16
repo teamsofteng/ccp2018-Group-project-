c5c6610525aeae7e58d24f7e915e452f2fc6567a
teamsofteng <37052413+teamsofteng@users.noreply.github.com>
2018-04-15 20:33:20 -0500

    Add files via upload

---
+++ firstPro/majors/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from classReview.forms import classReviewForm
+#from classReview.models import CourseLinks
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from scheduleGen.models import Majors
+class index(TemplateView):
+    template_name = 'majors/index.html'
+
+# Create your views here.
