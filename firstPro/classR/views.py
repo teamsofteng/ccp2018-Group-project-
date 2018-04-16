ed755ee7a2ee150afb47b81319871d588ba44ab7
teamsofteng <37052413+teamsofteng@users.noreply.github.com>
2018-04-16 17:09:21 -0500

    Add files via upload
    computer science major reviews

--- firstPro/classR/views.py
+++ firstPro/classR/views.py
@@ -5,11 +5,12 @@ from classReview.forms import classReviewForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from scheduleGen.models import Majors
+from scheduleGen.models import CourseLinks
 class index(TemplateView):
     template_name = 'classR/index.html'
     def get(self,request):
         posts = Majors.objects.filter(majorName="Computer Science")
-        post1 = CourseLinks.objects.raw("SELECT scheduleGen_courselinks.courseID, connCourseID, difficultyNode, cohesionNode, overallNode FROM scheduleGen_courses, scheduleGen_courselinks WHERE scheduleGen_courses.courseID = scheduleGen_courselinks.courseID;") 
+        post1 = CourseLinks.objects.raw('SELECT DISTINCT courseID, 1 id,connCourseID, difficultyNode, cohesionNode, overallNode FROM scheduleGen_courselinks WHERE scheduleGen_courselinks.courseID IN (SELECT DISTINCT(courseID) FROM scheduleGen_courselinks, scheduleGen_majors WHERE majorName = "Computer Science" AND instr(majorCourses, courseID)>0);') 
         args = {'posts':posts,'post1':post1}
         return render(request, self.template_name, args)
 
