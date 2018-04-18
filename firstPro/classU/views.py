b7ee9c84c3a8cf7f6e27290d2a469ef7f767b3a7
Sneh Koul <37052413+teamsofteng@users.noreply.github.com>
2018-04-17 22:48:02 -0500

    sql update

--- firstPro/classU/views.py
+++ firstPro/classU/views.py
@@ -10,7 +10,7 @@ class index(TemplateView):
     template_name = 'classU/index.html'
     def get(self,request):
         posts = Majors.objects.filter(majorName="Manangement Information Systems")
-        post1 = CourseLinks.objects.raw('SELECT DISTINCT courseID, 1 id,connCourseID, difficultyNode, cohesionNode, overallNode FROM scheduleGen_courselinks WHERE scheduleGen_courselinks.courseID IN (SELECT DISTINCT(courseID) FROM scheduleGen_courselinks, scheduleGen_majors WHERE majorName = "Management Information Systems" AND instr(majorCourses, courseID)>0);') 
+        post1 = CourseLinks.objects.raw('SELECT DISTINCT courseID, 1 id,connCourseID, difficultyNode, cohesionNode, overallNode FROM scheduleGen_courselinks WHERE scheduleGen_courselinks.courseID IN (SELECT DISTINCT(courseID) FROM scheduleGen_courselinks, scheduleGen_majors WHERE majorName = "Management Information Systems" AND (instr(majorCourses, courseID)>0 OR instr(sideCourses, courseID) >0 OR instr(electives,courseID) > 0));') 
         args = {'posts':posts,'post1':post1}
         return render(request, self.template_name, args)
 
