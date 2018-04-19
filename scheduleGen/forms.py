bc296bba4e29a6a93224e61d9d51e26439d5ecaf
CommFireSquid <36043419+CommFireSquid@users.noreply.github.com>
2018-04-19 03:27:26 -0500

    Add files via upload

--- scheduleGen/forms.py
+++ scheduleGen/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ScheduleForm(forms.Form):
-	majorChoices = [('','Select a Major'),('compsci','Computer Science'),('bioi','Bioinformatics')]
+	majorChoices = [('','Select a Major'),('compsci','Computer Science')]
 	searchValueMajor = forms.CharField(label='Select Your Major',widget=forms.Select(choices=majorChoices))
